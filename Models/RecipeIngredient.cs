26eead3497d3570d7fe6bb51f41e73c62d798529
alwright33 <austinlucaswright@gmail.com>
2025-01-21 17:12:58 -0600

    modified controllers for frontend compatability

--- Models/RecipeIngredient.cs
+++ Models/RecipeIngredient.cs
@@ -4,6 +4,7 @@ namespace Cookistry.Models;
 
 public class RecipeIngredient
 {
+    [Key]
     public int RecipeId { get; set; }
     public int IngredientId { get; set; }
     public decimal Quantity { get; set; }
